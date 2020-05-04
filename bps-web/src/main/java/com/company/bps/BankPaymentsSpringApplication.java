f630cc5cab82d997e845f3d326c184e633953f3d
OpenUEyes <romanenkoroman.dev@gmail.com>
2020-05-04 21:19:03 +0300

    init

---
+++ bps-web/src/main/java/com/company/bps/BankPaymentsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.company.bps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankPaymentsSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankPaymentsSpringApplication.class, args);
+    }
+
+}
